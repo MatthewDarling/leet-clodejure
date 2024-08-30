49d27cd357c1b5d30935b14c29bbf9dab758d9d6
Matthew Darling <matthewjdarling@gmail.com>
2024-08-30 17:37:13 -0400

    Tree inverting without building a proper tree
    Since LeetCode gives a flat vector as input and expects a flat vector as
    output, we don't really need to bother with a real tree structure.
    Instead, since we're flipping all the nodes at a given depth, we just
    need to call `reverse` on a coll of each "layer." So we reduce across
    the input list and make sure we're doubling the size of each previous
    step.
    
    I did mess around with a "proper" solution using `clojure.zip` and the
    node struct provided by LeetCode's sample code. I'll
    commit and comment on that separately.

--- src/leetcode/problems/226.clj
+++ src/leetcode/problems/226.clj
@@ -6,18 +6,33 @@
     The number of nodes in the tree is in the range [0, 100].
     -100 <= Node.val <= 100")
 
-(defrecord TreeNode [val left right])
+;; their binary tree structure:
+;; [     4,
+;;    2,    7,
+;;  1,3,    6,9]
 
-(defn make-tree-node
-  ([]
-   (make-tree-node 0))
-  ([val]
-   (make-tree-node val nil nil))
-  ([val left]
-   (make-tree-node val left nil))
-  ([val left right]
-   (->TreeNode val left right)))
+(defn start-new-tree-layer?
+  "Rather than operate on the flat vector input used by LeetCode, it's trivial to
+  invert the tree if each layer of nodes is its own vector. Since we're working
+  with a binary tree, we want to start a new layer of branches each time the
+  current layer is double the size of the previous layer.
+
+  Because the first layer has no preceding layer to compare against, it's always
+  `true` that a new layer should be started for the second element onwards. For
+  the second element onwards, return `true` if the last collection nested in
+  `coll` is double the length of its preceding element."
+  [coll]
+  (or (= 1 (count coll))
+      (= (* 2 (count (last (pop coll))))
+         (count (last coll)))))
 
 (defn solution
-  [tree-seq]
-  )
+  [[root & children]]
+  (let [layered (reduce (fn [acc item]
+                          (if (start-new-tree-layer? acc)
+                            (conj acc [item])
+                            (conj (pop acc) (conj (last acc) item))))
+                        (if root [[root]] [])
+                        children)
+        inverted-layers (map reverse layered)]
+    (flatten inverted-layers)))
