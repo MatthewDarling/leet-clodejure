c1f4376ed41c9024395fe1c773f79e53cf06b8a1
Matthew Darling <matthewjdarling@gmail.com>
2024-09-05 17:58:59 -0400

    Add helper function to create linked list from seq

--- test/leetcode/problems/876_test.clj
+++ test/leetcode/problems/876_test.clj
@@ -3,26 +3,19 @@
             [clojure.test :as t]))
 
 (t/deftest case1
-  (t/is (= (->> 5
-                sut/make-list-node
-                (sut/make-list-node 4)
-                (sut/make-list-node 3))
-           (sut/solution (->> 5
-                              sut/make-list-node
-                              (sut/make-list-node 4)
-                              (sut/make-list-node 3)
-                              (sut/make-list-node 2)
-                              (sut/make-list-node 1))))))
+  (t/is (= (sut/seq->linked-list [3 4 5])
+           (sut/solution (sut/seq->linked-list (range 1 6))))))
 
 (t/deftest case2
+  (t/is (= (sut/seq->linked-list [4 5 6])
+           (sut/solution (sut/seq->linked-list (range 1 7))))))
+
+(t/deftest my-cases
   (t/is (= (->> 6
                 sut/make-list-node
                 (sut/make-list-node 5)
-                (sut/make-list-node 4))
-           (sut/solution (->> 6
-                              sut/make-list-node
-                              (sut/make-list-node 5)
-                              (sut/make-list-node 4)
-                              (sut/make-list-node 3)
-                              (sut/make-list-node 2)
-                              (sut/make-list-node 1))))))
+                (sut/make-list-node 4)
+                (sut/make-list-node 3)
+                (sut/make-list-node 2)
+                (sut/make-list-node 1))
+           (sut/seq->linked-list (range 1 7)))))
