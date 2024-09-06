4aab33c0b2e63598c6826b31a194bea0ff6577d4
Matthew Darling <matthewjdarling@gmail.com>
2024-09-06 17:09:32 -0400

    Copy linked list helpers to delete nodes problem

--- test/leetcode/problems/3217_test.clj
+++ test/leetcode/problems/3217_test.clj
@@ -3,35 +3,27 @@
             [clojure.test :as t]))
 
 (t/deftest case1
-  (t/is (= (sut/make-list-node 4 (sut/make-list-node 5))
+  (t/is (= (sut/seq->linked-list [4 5])
            (sut/solution [1 2 3]
-                         (->> 5
-                              sut/make-list-node
-                              (sut/make-list-node 4)
-                              (sut/make-list-node 3)
-                              (sut/make-list-node 2)
-                              (sut/make-list-node 1))))))
+                         (sut/seq->linked-list (range 1 6))))))
 
 (t/deftest case2
-  (t/is (= (->> 2 sut/make-list-node (sut/make-list-node 2) (sut/make-list-node 2))
+  (t/is (= (sut/seq->linked-list [2 2 2])
            (sut/solution [1]
-                         (->> 2
-                              sut/make-list-node
-                              (sut/make-list-node 1)
-                              (sut/make-list-node 2)
-                              (sut/make-list-node 1)
-                              (sut/make-list-node 2)
-                              (sut/make-list-node 1))))))
+                         (sut/seq->linked-list [1 2 1 2 1 2])))))
 
 (t/deftest case3
-  (t/is (= (->> 4
+  (t/is (= (sut/seq->linked-list (range 1 5))
+           (sut/solution [5]
+                         (sut/seq->linked-list (range 1 5))))))
+
+(t/deftest my-cases
+  (t/is (= (->> 6
                 sut/make-list-node
+                (sut/make-list-node 5)
+                (sut/make-list-node 4)
                 (sut/make-list-node 3)
                 (sut/make-list-node 2)
                 (sut/make-list-node 1))
-           (sut/solution [5]
-                         (->> 4
-                              sut/make-list-node
-                              (sut/make-list-node 3)
-                              (sut/make-list-node 2)
-                              (sut/make-list-node 1))))))
+           (sut/seq->linked-list (range 1 7)))
+        "Helper function builds the same lists as a manual approach"))
