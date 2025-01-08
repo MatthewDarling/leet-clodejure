b5acf2fd2fa693ffe63b8ef52f706330a19b62c5
Matthew Darling <matthewjdarling@gmail.com>
2025-01-07 23:41:14 -0500

    Add old non-working WIP code
    Going to use this for some live coding with the CIDER debugger. Maybe
    we'll get it working!
    
    I'm pretty sure the problems all stem from the difference between `nil`
    and `(->TreeNode nil)`. Since I initially wrote the TreeNode stuff for a
    balanced binary tree, a node either had zero children, two, or was the
    sole node with one child for an uneven number of leaves. But
    intentionally creating unbalanced trees, as this problem requires, means
    you need a clear "this space intentionally left blank" marker. I've
    haphazardly changed some things to get closer to that, but I think it
    will be much simpler with a better design that accounts for this.

--- src/leetcode/problems/1367.clj
+++ src/leetcode/problems/1367.clj
@@ -9,7 +9,8 @@
 
     The number of nodes in the tree will be in the range [1, 2500].
     The number of nodes in the list will be in the range [1, 100].
-    1 <= Node.val <= 100 for each node in the linked list and binary tree.")
+    1 <= Node.val <= 100 for each node in the linked list and binary tree."
+  (:require [clojure.zip :as zip]))
 
 (defrecord ListNode [val next])
 
@@ -21,15 +22,140 @@
   ([val next-node]
    (->ListNode val next-node)))
 
+(defmethod print-method TreeNode
+  [node ^java.io.Writer w]
+  (.write w (format "node: %s [%s, %s]"
+                    (:val node)
+                    ;; Using pr-str allows the print method to recur with the child nodes
+                    (pr-str (:left node))
+                    (pr-str (:right node)))))
+
+(defn tree-node?
+  [obj]
+  (instance? TreeNode obj))
+
 (defn make-tree-node
+  "Converts the input, and any provided children, into `TreeNode`s."
   ([]
    (make-tree-node 0))
   ([val]
-   (make-tree-node val nil nil))
+   (cond
+     (tree-node? val) val
+     (nil? val) (->TreeNode nil nil nil)
+     :else (make-tree-node val nil nil)))
   ([val left]
    (make-tree-node val left nil))
   ([val left right]
-   (->TreeNode val left right)))
+   (->TreeNode val
+               (make-tree-node left)
+               (make-tree-node right))))
+
+(def child-seq (juxt :left :right))
+
+(defn children
+  [node]
+  (child-seq node))
+
+(defn branch?
+  "A node is a branch if it has more than 0 non-`nil` children."
+  [node]
+  (and (:val node)
+       (->> node
+            children
+            (remove (some-fn nil? (comp nil? :val)))
+            count
+            pos?)))
+
+(defn make-node
+  "`clojure.zip` takes a function to 'make new nodes,' but it's also used when
+  navigating the tree to resolve changed nodes. So this may be called with just
+  the `val` desired for a new `TreeNode`, or to update the children of an
+  existing `TreeNode`. Therefore, we'll use `make-tree-node` to coerce
+  `val-or-node`, and manually set the `left` and `right` nodes."
+  [val-or-node [left right]]
+  (let [as-tree-node (make-tree-node val-or-node)]
+    (assoc as-tree-node
+           :left (make-tree-node left)
+           :right (make-tree-node right))))
+
+(defn zip-wrapper
+  "A helper function to provide the right functional arguments to
+  `clojure.zip/zipper` for a `TreeNode` tree."
+  [root]
+  (zip/zipper branch?
+              children
+              make-node
+              (make-tree-node root)))
+
+;;; clojure.zip helper functions
+(defn top-of-tree
+  "The built-in `clojure.zip/root` function has this same logic, but also calls
+  `clojure.zip/node` at the end. So to navigate to the top of the tree, we need
+  our own function."
+  [loc]
+  (if-let [parent (zip/up loc)]
+    (recur parent)
+    loc))
+
+(defn child-locs
+  "The built-in `clojure.zip/children` function operates on a node, rather than a
+  `loc`. You can't really reconstruct the navigation data from just the node, so
+  if you want to process the children as `loc`s, you need this function
+  instead."
+  [loc]
+  (when-let [first-child (zip/down loc)]
+    (->> first-child
+         (iterate zip/right)
+         (take-while some?))))
+
+;;; Solution implementation
+(defn breadth-first-filter
+  "Like `filter`, but using a breadth-first search of the provided `zipper`.
+
+  `filter-loc` must be a function that receives a `clojure.zip` `loc` structure,
+  and returns a truthy value if you want to keep that `loc` in the output. A
+  `loc` is a vector with mandatory metadata and two elements: the `node`, and
+  the current zipper navigation data. You can filter on the node itself, and/or
+  the navigation data."
+  [zipper filter-loc]
+  (loop [result []
+         remaining-queue (conj clojure.lang.PersistentQueue/EMPTY zipper)]
+    (if (seq remaining-queue)
+      (let [current-loc (peek remaining-queue)
+            child-nodes (child-locs current-loc)]
+        (recur (if (filter-loc current-loc)
+                 (conj result current-loc)
+                 result)
+               (into (pop remaining-queue) child-nodes)))
+      result)))
+
+(defn seq->unbalanced-tree
+  [[root & children]]
+  (let [tree-initial (-> (zip-wrapper root)
+                         (zip/edit make-node (take 2 children)))]
+    (loop [tree-so-far tree-initial
+           remaining-children (drop 2 children)]
+      (if-not (seq remaining-children)
+        (top-of-tree tree-so-far)
+        (recur (-> tree-so-far
+                   top-of-tree
+                   (breadth-first-filter (every-pred (comp some? :val zip/node)
+                                                     (comp not zip/branch?)))
+                   first
+                   (zip/edit make-node (take 2 remaining-children)))
+               (drop 2 remaining-children))))))
+
+
+
+(comment
+  (= (zip/root (seq->unbalanced-tree [2 nil 2 nil 2 nil 1]))
+     (make-tree-node 2
+                     nil
+                     (make-tree-node 2
+                                     nil
+                                     (make-tree-node 2
+                                                     nil
+                                                     (make-tree-node 1))))))
 
 (defn solution
   [linked-list binary-tree])
--- test/leetcode/problems/1367_test.clj
+++ test/leetcode/problems/1367_test.clj
@@ -1,6 +1,18 @@
 (ns leetcode.problems.1367-test
   (:require [leetcode.problems.1367 :as sut]
-            [clojure.test :as t]))
+            [clojure.pprint :as pprint]
+            [clojure.test :as t]
+            [clojure.zip :as zip]))
+
+(defn seq->linked-list
+  "Convert the provided `seq-to-link` into a linked list of `ListNode`s, in the
+  same order as the original seq."
+  [seq-to-link]
+  (let [[reversed-head & reversed-tail] (reverse seq-to-link)]
+    (reduce (fn [prev-node new-val]
+              (sut/make-list-node new-val prev-node))
+            (sut/make-list-node reversed-head)
+            reversed-tail)))
 
 (def example-tree
   (sut/make-tree-node 1
@@ -15,17 +27,121 @@
                                                                                   (sut/make-tree-node 3))))))
 
 (t/deftest case1
-  (t/is (sut/solution (->> 8
-                           sut/make-list-node
-                           (sut/make-list-node 2)
-                           (sut/make-list-node 4))
+  (t/is (sut/solution (seq->linked-list [4 2 8])
                       example-tree)))
 
 (t/deftest case2
-  (t/is (sut/solution (->> 8
-                           sut/make-list-node
-                           (sut/make-list-node 6)
-                           (sut/make-list-node 2)
-                           (sut/make-list-node 4)
-                           (sut/make-list-node 1))
+  (t/is (sut/solution (seq->linked-list [1 4 2 6 8])
                       example-tree)))
+
+(t/deftest from-comments
+  (t/is (sut/solution (seq->linked-list [1 10])
+                      (zip/root (sut/seq->unbalanced-tree [1 nil 1 10 1 9]))))
+  (t/is (sut/solution (seq->linked-list [2 2 1])
+                      (zip/root (sut/seq->unbalanced-tree [2 nil 2 nil 2 nil 1])))))
+
+
+(t/deftest my-cases
+  (t/is (= (->> 6
+                sut/make-list-node
+                (sut/make-list-node 5)
+                (sut/make-list-node 4)
+                (sut/make-list-node 3)
+                (sut/make-list-node 2)
+                (sut/make-list-node 1))
+           (seq->linked-list (range 1 7)))
+        "Linked list helper function builds the same lists as a manual approach")
+  (t/is (= (zip/root (sut/seq->unbalanced-tree [2 nil 2 nil 2 nil 1]))
+           (sut/make-tree-node 2
+                               nil
+                               (sut/make-tree-node 2
+                                                   nil
+                                                   (sut/make-tree-node 2
+                                                                       nil
+                                                                       (sut/make-tree-node 1)))))
+        "Unbalanced tree helper function builds the same tree as a manual approach")
+  (t/is (= (sut/make-tree-node 1
+                               nil
+                               (sut/make-tree-node 1
+                                                   (sut/make-tree-node 10 (sut/make-tree-node 9))
+                                                   (sut/make-tree-node 1)))
+           (zip/root (sut/seq->unbalanced-tree [1 nil 1 10 1 9])))
+        "Builds same tree")
+  (t/is (= (sut/make-tree-node 1
+                      (sut/make-tree-node 4
+                                          (sut/make-tree-node 2
+                                                              (sut/make-tree-node 1)))
+                      (sut/make-tree-node 4
+                                          (sut/make-tree-node 2
+                                                              (sut/make-tree-node 6)
+                                                              (sut/make-tree-node 8
+                                                                                  (sut/make-tree-node 1)
+                                                                                  (sut/make-tree-node 3)))))
+           (zip/root (sut/seq->unbalanced-tree [1
+                                                4     4
+                                                2 nil 2 nil
+                                                1 nil   6       8
+                                                nil nil nil nil 1 3])))
+        "Builds same tree as their example"))
+
+(def expected {:val 1,
+               :left
+               {:val 4,
+                :left
+                {:val 2,
+                 :left
+                 {:val 1,
+                  :left {:val nil, :left nil, :right nil},
+                  :right {:val nil, :left nil, :right nil}},
+                 :right {:val nil, :left nil, :right nil}},
+                :right {:val nil, :left nil, :right nil}},
+               :right
+               {:val 4,
+                :left
+                {:val 2,
+                 :left
+                 {:val 6,
+                  :left {:val nil, :left nil, :right nil},
+                  :right {:val nil, :left nil, :right nil}},
+                 :right
+                 {:val 8,
+                  :left
+                  {:val 1,
+                   :left {:val nil, :left nil, :right nil},
+                   :right {:val nil, :left nil, :right nil}},
+                  :right
+                  {:val 3,
+                   :left {:val nil, :left nil, :right nil},
+                   :right {:val nil, :left nil, :right nil}}}},
+                :right {:val nil, :left nil, :right nil}}})
+
+(def actual   {:val 1,
+               :left
+               {:val 4,
+                :left
+                {:val 2,
+                 :left
+                 {:val 1,
+                  :left
+                  {:val 1,
+                   :left {:val nil, :left nil, :right nil},
+                   :right {:val nil, :left nil, :right nil}},
+                  :right
+                  {:val 3,
+                   :left {:val nil, :left nil, :right nil},
+                   :right {:val nil, :left nil, :right nil}}},
+                 :right {:val nil, :left nil, :right nil}},
+                :right {:val nil, :left nil, :right nil}},
+               :right
+               {:val 4,
+                :left
+                {:val 2,
+                 :left
+                 {:val 6,
+                  :left {:val nil, :left nil, :right nil},
+                  :right {:val nil, :left nil, :right nil}},
+                 :right
+                 {:val 8,
+                  :left {:val nil, :left nil, :right nil},
+                  :right {:val nil, :left nil, :right nil}}},
+                :right {:val nil, :left nil, :right nil}}})
